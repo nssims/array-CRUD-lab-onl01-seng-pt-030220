21b2708c0b42896884bab852d6cd2d0f460edbc1
Nicole Sims <nssims105@gmail.com>
2020-03-14 20:53:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,34 +1,35 @@
 def create_an_empty_array
-[]
-end  
+  array = []
+end
+
 def create_an_array
-["horses","cows","pigs","chickens"]
-end  
+  array = ["Bun", "Cheese", "Patty", "Ketchup"]
+end
 
-#def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array, element)
+  array = array.push(element)
+end
 
+def add_element_to_start_of_array(array, element)
+  array = array.unshift(element)
+end
 
-#def add_element_to_start_of_array(array, element)
-  
+def remove_element_from_end_of_array(array)
+  array = array.pop()
+end
 
+def remove_element_from_start_of_array(array)
+  array = array.shift()
+end
 
-#def remove_element_from_end_of_array(array)
-  
+def retrieve_element_from_index(array, index_number)
+  array[index_number]
+end
 
+def retrieve_first_element_from_array(array)
+  array.first
+end
 
-#def remove_element_from_start_of_array(array)
-  
-
-
-#def retrieve_element_from_index(array, index_number)
-  
-
-
-#def retrieve_first_element_from_array(array)
-  
-
-
-#def retrieve_last_element_from_array(array)
-  
-
+def retrieve_last_element_from_array(array)
+  array.last
+end
